00ff5e4824394a8b4741abbf26a10b7fd2fa28b1
Maria Kasińska <mariakasinska17@gmail.com>
2021-09-16 23:26:56 +0200

    fix bugs /without diagonal mode

--- connect4/script.js
+++ connect4/script.js
@@ -9,22 +9,24 @@ class Connect4 {
     for (let column of this.columns) {
       if (column.toString().indexOf('p1,p1,p1,p1') > -1) this.isWon = true;
       if (column.toString().indexOf('p2,p2,p2,p2') > -1) this.isWon = true;
-      for (let element of column) {
-        if (
-          element ==
-            this.columns[this.columns.indexOf(column + 1)][
-              column.indexOf(element)
-            ] &&
-          element ==
-            this.columns[this.columns.indexOf(column + 2)][
-              column.indexOf(element)
-            ] &&
-          element ==
-            this.columns[this.columns.indexOf(column + 3)][
-              column.indexOf(element)
-            ]
-        ) {
-          this.isWon = true;
+      if (this.columns.indexOf(column) <= 3) {
+        for (let element of column) {
+          if (
+            element ==
+              this.columns[this.columns.indexOf(column) + 1][
+                column.indexOf(element)
+              ] &&
+            element ==
+              this.columns[this.columns.indexOf(column) + 2][
+                column.indexOf(element)
+              ] &&
+            element ==
+              this.columns[this.columns.indexOf(column) + 3][
+                column.indexOf(element)
+              ]
+          ) {
+            this.isWon = true;
+          }
         }
       }
     }
@@ -54,6 +56,3 @@ class Connect4 {
     }
   }
 }
-
-let newGame = new Connect4();
-newGame.play(3);
